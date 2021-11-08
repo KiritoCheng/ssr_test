d10c570a7dd921c5d93744e3f39a187eea8a3c95
Kirito <1345036885@qq.com>
2021-11-08 11:31:27 +0800

    update port

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 
-console.log(path.join(__dirname, "pages"));
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "pages")],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config;
   },
 };
